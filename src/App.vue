4cdb781c24bdb5f25c6d862676208c6d998837c0
robinmihkelson <rmihkelson06@gmail.com>
2025-05-28 11:33:37 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Page1 from './components/Page1.vue'
+</script>
+
+<template>
+  <Page1/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Page1.vue
@@ -0,0 +1,75 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-full h-32 bg-black">
+        <div class="flex gap-2 pt-8 px-6 text-white">
+            <img class="h-12 w-12" src="/src/assets/img/img1.png" alt="">
+            <button class="text-4xl font-space-grotesk font-bold">Optinet</button>
+            <div class="flex">
+                <button class="text-[17px] font-space-grotesk px-8 pt-[16px]">SOLUTIONS</button>
+                <button class="text-[17px] font-space-grotesk px-8 pt-[12px]">ZERO-LOSS FAILOVER</button>
+                <button class="text-[17px] font-space-grotesk px-8 pt-[12px]">MULTI-TENANT</button>
+                <button class="text-[17px] font-space-grotesk px-8 pt-[12px]">ENTERPRISE NNI</button>
+                <div class="flex mx-[220px]">
+                    <button class="text-[17px] font-space-grotesk px-8 pt-[12px]">LEARN</button>
+                    <button class="text-[17px] font-space-grotesk px-8 pt-[12px]">SUPPORT</button>
+                    <button class="h-16 text-[17px] font-space-grotesk px-8 pt-[7px] rounded-[90px] bg-[linear-gradient(45deg,#40DB5C,#03C7FD,#40DB5C)]">MAKE AN ENQUIRY</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="bg-center h-screen w-full bg-cover" style="background-image: url('/src/assets/img/bg.png');">
+    </section>
+
+    <div class="font-space-grotesk font-bold flex gap-4 justify-center mt-28">
+        <img class="mt-[3px] h-4 w-4"src="/src/assets/img/arrow01.png" alt="">
+        <h1>SOLUTIONS</h1>
+    </div>
+
+    <div class="font-space-grotesk text-center text-[40px] mt-6">
+        <h1>Productivity solutions that <span class="font-bold">capitalise</span></h1>
+        <h1>on your network <span class="font-bold">investment</span></h1>
+    </div>
+
+    <div class="flex flex-row">
+        <div class="mt-32 mx-52">
+            <img src="/src/assets/img/img2.png" alt="">
+        </div>
+
+        <div class="font-space-grotesk">
+            <div class="flex mt-48 gap-2">
+                <img class="w-10 h-8 mt-1" src="/src/assets/img/arrow02.png" alt="">
+                <h1 class="font-bold text-[29px]">CONNECTIVITY</h1>
+            </div>
+
+            <div class="w-[620px] h-8">
+                <h1 class="text-[#5D5D5D] mt-6 mx-[49px]">Last mile or first mile, fibre or copper, Ethernet or wireless, 20Mbs or 100Gbs - we'll always find the best-performing connectivity to fit your needs and location.</h1>
+            </div>
+
+            <button class="mt-20 w-[220px] h-[75px] text-white bg-black rounded-[40px]">LEARN MORE</button>
+
+            <div class="mt-24">
+                <hr>
+            </div>
+
+            <h1 class="text-[30px]">NETWORK TECHNOLOGIES</h1>
+
+            <div class="mt-5">
+                <hr>
+            </div>
+
+            <h1 class="text-[30px]">NETWORK TECHNOLOGIES</h1>
+
+            <div class="mt-5">
+                <hr>
+            </div>
+
+            <h1 class="text-[30px]">NETWORK TECHNOLOGIES</h1>
+        </div>
+    </div>
+
+
+</template>
